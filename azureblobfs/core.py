f6ec38733d7a21981a73a03b13bb266453215ed5
Manish Sinha <manish.sinha@xamarin.com>
2018-11-19 19:28:53 -0500

    Implemented basic DaskAzureBlobFileSystem minus open method

---
+++ azureblobfs/core.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#
+# The MIT License
+#
+# Copyright (c) 2018 Microsoft Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os
+import fnmatch
+
+from azure.storage.blob.blockblobservice import BlockBlobService
+
+import dask.bytes.core
+
+ab_protocol = "abfs"
+
+class DaskAzureBlobFileSystem(BlockBlobService):
+    def __init__(self, account_name=None, account_key=None, sas_token=None, **kwargs):
+        account_name = account_name or os.environ.get("AZURE_BLOB_ACCOUNT_NAME")
+        account_key = account_key or os.environ.get("AZURE_BLOB_ACCOUNT_KEY")
+        sas_token = sas_token or os.environ.get("AZURE_BLOB_SAS_TOKEN")
+        super(BlockBlobService, self).__init__(account_name=account_name,
+                                               account_key=account_key,
+                                               sas_token=sas_token,
+                                               protocol=kwargs.get("protocol"),
+                                               endpoint_suffix=kwargs.get("endpoint_suffix"),
+                                               custom_domain=kwargs.get("custom_domain"))
+
+    def glob(self, path):
+        container, blob_pattern = DaskAzureBlobFileSystem.split_container_blob(path)
+        return filter(lambda x: fnmatch.fnmatch(x.name, blob_pattern), self.list_blobs(container))
+
+    def mkdirs(self, path, **kwargs):
+        container, blob_pattern = DaskAzureBlobFileSystem.split_container_blob(path)
+        if not self.exists(container):
+            self.create_container(container, kwargs)
+
+    def open(self, path, mode='rb', **kwargs):
+        pass
+
+    def size(self, path):
+        container, blob_pattern = DaskAzureBlobFileSystem.split_container_blob(path)
+        return self.get_blob_properties(container, blob_pattern).properties.content_length
+
+    def ukey(self, path):
+        container, blob_pattern = DaskAzureBlobFileSystem.split_container_blob(path)
+        return self.get_blob_properties(container, blob_pattern).properties.etag
+
+    @classmethod
+    def split_container_blob(self, path):
+        index_sep = path.find("/")
+        if index_sep < 0:
+            raise Exception("The path provided is not in the format {protocol}://container/blob_pattern".format(protocol=ab_protocol))
+        return path[:index_sep], path[index_sep+1:]
+
+dask.bytes.core._filesystems[ab_protocol] = DaskAzureBlobFileSystem
