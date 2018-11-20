d06f9b9c1bda58a1348930e2ce736023516a8b3e
Manish Sinha <manish.sinha@xamarin.com>
2018-11-20 11:40:28 -0500

    Added test case to test DaskAzureBlobFileSystem: size and ukey

--- tests/test_azureblobfs.py
+++ tests/test_azureblobfs.py
@@ -33,8 +33,28 @@ class DaskAzureBlobFileSystemTest(unittest.TestCase):
 
     account_name="e29"
     container_name = "abfs-methods"
+    files = { "Local_Weather_Data.csv": {"size": 7580289, "ukey": "\"0x8D64E894851407E\""},
+              "rdu-weather-history.csv": {"size": 480078, "ukey": "\"0x8D64E894B716BBD\""}}
+    def setUp(self):
+        self.fs = DaskAzureBlobFileSystem(account_name=self.account_name)
+
     def test_glob(self):
-        fs = DaskAzureBlobFileSystem(account_name=self.account_name)
-        all_files = [file.name for file in fs.glob("{container_name}/*.csv".format(container_name=self.container_name))]
-        self.assertIn("Local_Weather_Data.csv", all_files)
-        self.assertIn("rdu-weather-history.csv", all_files)
+        all_files = [file.name for file in self.fs.glob("{container_name}/*.csv".format(container_name=self.container_name))]
+        for file in self.files:
+            self.assertIn(file, all_files)
+
+    def test_ukey(self):
+        for file, props in self.files.items():
+            found_ukey = self.fs.ukey("{container}/{blob}".format(container=self.container_name, blob=file))
+            expected_ukey =  props["ukey"]
+            self.assertEqual(found_ukey, expected_ukey,
+                             "For blob '{blob}', expected ukey to be '{expected_ukey}' but found '{found_ukey}'".format(
+                                 blob=file, expected_ukey=expected_ukey, found_ukey=found_ukey))
+
+    def test_size(self):
+        for file, props in self.files.items():
+            found_size = self.fs.size("{container}/{blob}".format(container=self.container_name, blob=file))
+            expected_size =  props["size"]
+            self.assertEqual(found_size, expected_size,
+                             "For blob '{blob}', expected size to be '{expected_size}' but found '{found_size}'".format(
+                                 blob=file, expected_size=expected_size, found_size=found_size))
