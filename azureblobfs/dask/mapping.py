7ffcc14fa6f04a377a862e365c9f08b8cac67d0a
Manish Sinha <manish.sinha@xamarin.com>
2018-11-27 17:56:52 -0500

    Implement AzureBlobMap

--- azureblobfs/dask/mapping.py
+++ azureblobfs/dask/mapping.py
@@ -22,6 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+import pickle
 from collections import MutableMapping
 
 from azureblobfs.dask import DaskAzureBlobFileSystem
@@ -29,33 +30,84 @@ from azureblobfs.dask import DaskAzureBlobFileSystem
 class AzureBlobMap(MutableMapping):
     def __init__(self, location, fs):
         if not isinstance(fs, DaskAzureBlobFileSystem):
-            raise TypeError("Argument fs needs to be of type azureblobfs.dask.DaskAzureBlobFileSystem")
+            raise TypeError("Argument 'fs' needs to be of type azureblobfs.dask.DaskAzureBlobFileSystem")
         self.location = location
         self.fs = fs
 
     def clear(self):
-        pass
+        for matches_entry in self.fs.glob(self.location):
+            container, blob = self.fs.split_container_blob(matches_entry)
+            self.fs.rm(container, blob)
 
     def get(self, key, default_value=None):
-        pass
+        try:
+            return self[key]
+        except:
+            return default_value
 
     def items(self):
-        pass
+        return list(map(lambda key: (key, self.__getitem__(key)), self.keys()))
 
     def keys(self):
-        pass
+        return list(self.__iter__())
 
     def pop(self, key, default_value=None):
-        pass
+        try:
+            value = self[key]
+            del self[key]
+            return value
+        except KeyError:
+            if default_value is not None:
+                return default_value
+            raise
 
     def popitem(self):
-        pass
+        key = timestamp = None
+        for matches_entry in self.fs.glob(self.location):
+            container, blob = self.fs.split_container_blob(matches_entry)
+            last_modified = self.fs.last_modified(container, blob)
+            if timestamp is None or last_modified > timestamp:
+                timestamp = last_modified
+                key = matches_entry
+        if key is None:
+            raise KeyError()
+        prefix = self.fs.join_container_blob(self.location, "")
+        key = key.replace(prefix, "")
+        return (key, self.pop(key))
 
     def setdefault(self, key, default_value=None):
-        pass
-
-    def update(self, key, **value):
-        pass
+        try:
+            return self[key]
+        except KeyError:
+            self[key] = default_value
+            return default_value
 
     def values(self):
-        pass
+        return list(map(self.__getitem__, self.__iter__()))
+
+    def __setitem__(self, key, value):
+        container, blob_prefix = DaskAzureBlobFileSystem.split_container_blob(self.location)
+        blob_path = DaskAzureBlobFileSystem.join_container_blob(blob_prefix, key)
+        self.fs.connection.create_blob_from_bytes(container, blob_path, pickle.dumps(value))
+
+    def __delitem__(self, key):
+        container, blob_prefix = DaskAzureBlobFileSystem.split_container_blob(self.location)
+        blob_path = DaskAzureBlobFileSystem.join_container_blob(blob_prefix, key)
+        if not self.fs.exists(container, blob_path):
+            raise KeyError (key)
+        self.fs.rm(container, blob_path)
+
+    def __getitem__(self, key):
+        full_path = self.fs.join_container_blob(self.location, key)
+        container, blob_path = DaskAzureBlobFileSystem.split_container_blob(full_path)
+        if not self.fs.exists(container, blob_path):
+            raise KeyError (key)
+        with self.fs.open(full_path) as f:
+            return pickle.loads(f.read())
+
+    def __iter__(self):
+        prefix = self.fs.join_container_blob(self.location, "")
+        return map(lambda x: x.replace(prefix, ""), self.fs.glob(self.location))
+
+    def __len__(self):
+        return len([_ for _ in self.fs.glob(self.location)])
