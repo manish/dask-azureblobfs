80bce9315f14d13a63c4e4d8e9c44474073dcf4c
Manish Sinha <manish.sinha@xamarin.com>
2018-11-27 22:09:43 -0500

    Fix flake8 tests in test_azureblobfs

--- tests/test_azureblobfs.py
+++ tests/test_azureblobfs.py
@@ -4,13 +4,13 @@
 """Tests for `azureblobfs` package."""
 
 import unittest
-import azureblobfs
 import warnings
 
 import dask.bytes.core
 
-from azureblobfs import DaskAzureBlobFileSystem
+from azureblobfs.dask import DaskAzureBlobFileSystem
 from azureblobfs.utils import generate_guid
+
 
 class SplitContainerBlobTest(unittest.TestCase):
 
@@ -21,30 +21,31 @@ class SplitContainerBlobTest(unittest.TestCase):
         warnings.simplefilter("ignore", ResourceWarning)
 
     def test_simple_scenario_success(self):
-        container, blob = azureblobfs.DaskAzureBlobFileSystem.split_container_blob("{container}/{blob_name}".format(
+        container, blob = DaskAzureBlobFileSystem.split_container_blob("{container}/{blob_name}".format(
             container=self.container, blob_name=self.blob_name))
         self.assertEqual(container, self.container)
         self.assertEqual(blob, self.blob_name)
 
     def test_subfolder_scenario_success(self):
-        container, blob = azureblobfs.DaskAzureBlobFileSystem.split_container_blob("{container}/{subfolder}/{blob_name}".format(
+        container, blob = DaskAzureBlobFileSystem.split_container_blob("{container}/{subfolder}/{blob_name}".format(
             container=self.container, subfolder=self.subfolder, blob_name=self.blob_name))
         self.assertEqual(container, self.container)
         self.assertEqual(blob, "{subfolder}/{blob_name}".format(subfolder=self.subfolder, blob_name=self.blob_name))
 
     def test_no_blob_failure(self):
-        with self.assertRaises(Exception) as context:
-            azureblobfs.DaskAzureBlobFileSystem.split_container_blob(self.container)
+        with self.assertRaises(Exception):
+            DaskAzureBlobFileSystem.split_container_blob(self.container)
 
     def test_is_registered(self):
         self.assertIn(DaskAzureBlobFileSystem.protocol, dask.bytes.core._filesystems)
 
-class DaskAzureBlobFileSystemTest(unittest.TestCase):
 
-    account_name="e29"
+class DaskAzureBlobFileSystemTest(unittest.TestCase):
+    account_name = "e29"
     container_name = "abfs-methods"
-    files = { "abfs-methods/Local_Weather_Data.csv": {"size": 7580289, "ukey": "\"0x8D64E894851407E\""},
-              "abfs-methods/rdu-weather-history.csv": {"size": 480078, "ukey": "\"0x8D64E894B716BBD\""}}
+    files = {"abfs-methods/Local_Weather_Data.csv": {"size": 7580289, "ukey": "\"0x8D64E894851407E\""},
+             "abfs-methods/rdu-weather-history.csv": {"size": 480078, "ukey": "\"0x8D64E894B716BBD\""}}
+
     def setUp(self):
         self.fs = DaskAzureBlobFileSystem(account_name=self.account_name)
         warnings.simplefilter("ignore", ResourceWarning)
@@ -57,7 +58,7 @@ class DaskAzureBlobFileSystemTest(unittest.TestCase):
     def test_ukey(self):
         for file, props in self.files.items():
             found_ukey = self.fs.ukey(file)
-            expected_ukey =  props["ukey"]
+            expected_ukey = props["ukey"]
             self.assertEqual(found_ukey, expected_ukey,
                              "For blob '{blob}', expected ukey to be '{expected_ukey}' but found '{found_ukey}'".format(
                                  blob=file, expected_ukey=expected_ukey, found_ukey=found_ukey))
@@ -65,7 +66,7 @@ class DaskAzureBlobFileSystemTest(unittest.TestCase):
     def test_size(self):
         for file, props in self.files.items():
             found_size = self.fs.size(file)
-            expected_size =  props["size"]
+            expected_size = props["size"]
             self.assertEqual(found_size, expected_size,
                              "For blob '{blob}', expected size to be '{expected_size}' but found '{found_size}'".format(
                                  blob=file, expected_size=expected_size, found_size=found_size))
